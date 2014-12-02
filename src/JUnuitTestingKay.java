ff17ba222e82846681f3347106768fbb2a54fb07
Fares Almuhanna <almuhanf@my.erau.edu>
2014-12-01 23:40:50 -0500

    Added JUnitTestingFares :)

--- src/JUnuitTestingKay.java
+++ src/JUnuitTestingKay.java
@@ -6,7 +6,7 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-import stringAnalyzer.Analyzer;
+//import stringAnalyzer.Analyzer;
 
 /**
  * Required JUnit Testings (2 per member)
